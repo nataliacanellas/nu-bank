a417ec87558e590ed4f1b5f34dd5bf66fc35610f
Natalia F. Canellas <nataliafcanellas@gmail.com>
2022-02-03 11:23:12 -0300

    ✨ Added button components

---
+++ components/Button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { 
+  Container,
+  Image,
+  Title,
+ } from './styles';
+
+const Button = ({text, image}) => {
+  return (
+    <Container>
+      <Image source={image}/>
+      <Title>{text}</Title>
+    </Container>
+  );
+}
+
+export default Button;
---
+++ components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  margin-right: 18px;
+  align-items: center;
+
+`;
+
+export const Image = styled.Image`
+  width: 60px;
+  height:60px;
+  background: #f0f1f5;
+  border-radius:50px;
+  margin-bottom: 8px;
+`;
+
+export const Title = styled.Text`
+  text-align: center;
+`;
