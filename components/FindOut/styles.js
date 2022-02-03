288aaa1890a11ca6a5b2eda4c5d27def2487e4bd
Natalia F. Canellas <nataliafcanellas@gmail.com>
2022-02-03 11:27:15 -0300

    ✨ Added Find out more info

---
+++ components/FindOut/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding:20px;
+  margin-left:-10px;
+`;
+
+export const Box = styled.TouchableOpacity`;
+  background: #f1f2f5;
+  width:200px;
+  height:250px;
+  border-radius:8px;
+`;
+export const BoxImage = styled.View`;
+
+`;
+export const Image = styled.Image`;
+  width:200px;
+  height:100px;
+`;
+export const BoxText = styled.View`;
+  padding:10px;
+`;
+export const Title = styled.Text`;
+  font-size:16px;
+  margin-top:5px;
+`;
+export const Text = styled.Text`;
+  font-size:14px;
+  color: #7c7d8b;
+  margin-top:5px;
+`;
